53ac3ad60fa7183fd31f167cce42753f29cdc11d
Noel Lotz <noel.lotz@pearson.com>
2023-01-19 18:28:29 -0600

    abel11: finalized work on player component

--- busPub/projects/abel11-app/src/app/player/player/player.component.ts
+++ busPub/projects/abel11-app/src/app/player/player/player.component.ts
@@ -1,25 +1,37 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
 
   @Input() steps: number = 0;
   @Input() mode: number = 0;
+  nextDisabled = false;
+  backDisabled = true;
 
   @Output() modeChange = new EventEmitter<number>();
 
-  public stepper(increase: boolean) {
-    if (increase) {
-      this.mode = this.mode + 1;
-    } else {
-      this.mode = this.mode - 1;
-    }
-    this.modeChange.emit(this.mode);
+  ngOnInit(): void {
+
+    this.nextDisabled = this.mode + 1 === this.steps ? this.nextDisabled = true : this.nextDisabled = false;
+    this.backDisabled = this.mode === 0 ? this.backDisabled = true : this.backDisabled = false;
 
   }
+
+  public stepper(increase: boolean) {
+    this.mode = increase ? this.mode + 1 : this.mode - 1;
+    this.modeChange.emit(this.mode);
+    if (this.mode + 1 === this.steps) {
+      this.nextDisabled = true
+    } else { this.nextDisabled = false; }
+
+    if (this.mode === 0) {
+      this.backDisabled = true;
+    } else { this.backDisabled = false}
+  }
+
 
 }
