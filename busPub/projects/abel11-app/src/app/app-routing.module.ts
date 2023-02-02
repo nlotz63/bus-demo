1ee2786c64786de3094f0d5eb50e0b6f865c0417
Noel Lotz <noel.lotz@pearson.com>
2023-02-02 17:13:46 -0600

    abel11: set default route to key-diagram3

--- busPub/projects/abel11-app/src/app/app-routing.module.ts
+++ busPub/projects/abel11-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/ch03/unemployment', pathMatch: 'full' },
+  { path: '', redirectTo: '/module02/key-diagram3?showPlayer=true', pathMatch: 'full' },
   { path: 'ch03', loadChildren: () => import('./chapter03/chapter03.module').then(m => m.Chapter03Module) },
   { path: 'module02', loadChildren: () => import('./module2/module2.module').then(m => m.Module2Module) }
 
