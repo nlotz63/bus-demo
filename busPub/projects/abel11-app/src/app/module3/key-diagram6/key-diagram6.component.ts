bdf1e45e0c695b36dc4c91b9a222cf45b03668bc
Noel Lotz <noel.lotz@pearson.com>
2023-02-21 18:17:53 -0600

    Abel11 key-diagram 6: Adjusted model to new specks. Added bullet points. Working on animation for restoring long-run equilibrium (partially complete)

--- busPub/projects/abel11-app/src/app/module3/key-diagram6/key-diagram6.component.ts
+++ busPub/projects/abel11-app/src/app/module3/key-diagram6/key-diagram6.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { interval } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { transition, trigger, style, animate } from '@angular/animations';
@@ -38,24 +39,24 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
   showPlayer: boolean = false;
 
   slider = new FormGroup({
-    expectedOutput: new FormControl(150),
-    wealth: new FormControl(150),
+    expectedOutput: new FormControl(75),
+    wealth: new FormControl(100),
     govPurchases: new FormControl(600),
     taxes: new FormControl(400),
-    expectedTFP: new FormControl(385),
+    expectedTFP: new FormControl(3558),
     effTax: new FormControl(0.12),
 
-    money: new FormControl(133200),
-    priceLevel: new FormControl(120),
+    money: new FormControl(255000),
+    priceLevel: new FormControl(100),
     expectedInflation: new FormControl(0.05),
     nominalRate: new FormControl(0.02),
 
     supplyShock: new FormControl(0),
     laborSupply: new FormControl(0),
     capitalStock: new FormControl(0)
-
-
   });
+
+  playInterval = interval(300);
 
   chart!: Highcharts.Chart;
 
@@ -88,15 +89,15 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
   public playStep(mode: number) {
     this.mode = mode;
     this.slider.setValue({
-      expectedOutput: 150,
-    wealth: 150,
+    expectedOutput: 75,
+    wealth: 100,
     govPurchases: 600,
     taxes: 400,
-    expectedTFP: 385,
+    expectedTFP: 3558,
     effTax: 0.12,
 
-    money: 133200,
-    priceLevel: 120,
+    money: 255000,
+    priceLevel: 100,
     expectedInflation: 0.05,
     nominalRate:0.02,
 
@@ -105,6 +106,22 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
     capitalStock: 0
     })
     this._setupChart();
+
+  }
+
+  public restoreEquilibrium() {
+    let newPrice: number;
+    this.playInterval.subscribe(() => {
+      let series = this._createSeries(false);
+      let difference = series.FE[0][0] - series.EQ[1].x;
+      if (difference < 0) {
+        newPrice = this.slider.value.priceLevel! + 1;
+      } else {
+       newPrice = this.slider.value.priceLevel! - 1;
+      }
+      this.updateChart({ priceLevel: newPrice });
+      console.log(difference);
+      })
 
   }
 
@@ -167,8 +184,8 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
         lineWidth: 1.,
         tickColor: '#757575',
         title: { useHTML: true, text: 'Output, Y (billions of dollars)' },
-        min: 0,
-        max: 2400
+        min: 1000,
+        max: 7000
 
       },
       yAxis: {
@@ -178,8 +195,8 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
         tickColor: '#757575',
         tickWidth: 1,
         title: { useHTML: true, text: 'Real interest rate, r' },
-        min: 0,
-        max: 2.5
+        min: -2.1,
+        max: 5
 
       },
       plotOptions: {
@@ -201,13 +218,13 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
     // slider values
     let slider = this.slider.value;
     // model parameters
-    let c0: number = slider.expectedOutput! + slider.wealth!, cy: number = .75, cr: number = 300, t0: number = 0.25 * slider.taxes!, t: number = 0.2,
+    let c0: number = slider.expectedOutput! + slider.wealth!, cy: number = .6, cr: number = 300, t0: number = 0.5 * slider.taxes!, t: number = 0.2,
     i0: number = slider.expectedTFP!*(1 - 4*slider.effTax!), ir: number = 200, G: number = slider.govPurchases!;
     let M: number = slider.money!, P: number = slider.priceLevel!, l0: number = slider.nominalRate! * 10000 + 800, ly: number = 0.5, lr: number = 500, piE: number = slider.expectedInflation! * 10 - 0.45;
 
-    let ybar = 1289 + slider.capitalStock! + slider.supplyShock! + slider.laborSupply!;
+    let ybar = 4000 + slider.capitalStock! + slider.supplyShock! + slider.laborSupply!;
 
-    let x: number = 300, isSeries = [], lmSeries = [], eqSeries = [], feSeries;
+    let x: number = 900, isSeries = [], lmSeries = [], eqSeries = [], feSeries;
 
     let is = (x: number) => { return (c0 + G + i0 - cy * t0 - x * (1 - cy + cy * t)) / (cr + ir); }
     let lm = (x: number) => { return (-M + l0 * P - lr * P * piE + ly * P * x) / (lr * P); }
@@ -228,7 +245,7 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
       isSeries.push(point);
       lmSeries.push(point2);
       x = x + 25;
-    } while (x < 2500);
+    } while (x < 7000);
 
     // equilibrium series
     eqSeries = [
@@ -241,15 +258,14 @@ export class KeyDiagram6Component implements OnInit, AfterViewInit {
         marker: {
           symbol: 'circle',
           enabled: true
-
         }
       },
-      { x: eq, y: 0, marker: {enabled: false}}
+      { x: eq, y: -3, marker: {enabled: false}}
     ];
 
     feSeries = [
-      [ybar, 0],
-      [ybar, 2.25]
+      [ybar, -3],
+      [ybar, 5]
     ]
 
     return {
