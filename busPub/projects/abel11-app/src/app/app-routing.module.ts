40c7940bdb62368b316dd12ff873737a35fcfb29
Noel Lotz <noel.lotz@pearson.com>
2023-03-01 15:33:14 -0600

    Abel 11: Added a dashboard

--- busPub/projects/abel11-app/src/app/app-routing.module.ts
+++ busPub/projects/abel11-app/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/module02/key-diagram3?showPlayer=true', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'module01', loadChildren: () => import('./chapter03/chapter03.module').then(m => m.Chapter03Module) },
   { path: 'module02', loadChildren: () => import('./module2/module2.module').then(m => m.Module2Module) },
-  { path: 'module03' , loadChildren: () => import('./module3/module3.module').then(m => m.Module3Module) }
+  { path: 'module03' , loadChildren: () => import('./module3/module3.module').then(m => m.Module3Module) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule ), title: 'Dashboard' }
 
 ];
 
---
+++ busPub/projects/abel11-app/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+  pf01 = false;
+  ld01 = false;
+  unemployment = false;
+  key3 = false;
+  key4 = false;
+  key5 = false;
+  solow = false;
+  fig92 = false;
+  key6 = false;
+  key7 = false;
+
+
+}
