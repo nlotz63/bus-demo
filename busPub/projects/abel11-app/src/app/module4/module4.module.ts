afa30be7598f1b213cbcfa39ccc38bb270da46ae
Noel Lotz <noel.lotz@pearson.com>
2023-04-12 17:44:11 -0500

    abel11: created balsic interactive functionality for figures 13.4, 13.5 and 13.9

--- busPub/projects/abel11-app/src/app/module4/module4.module.ts
+++ busPub/projects/abel11-app/src/app/module4/module4.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BusPubLibModule } from 'bus-pub-lib';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms'
+import { PlayerModule } from '../player/player.module';
 
 import { Module4RoutingModule } from './module4-routing.module';
 import { Fig134Component } from './fig134/fig134.component';
@@ -15,7 +19,12 @@ import { Fig139Component } from './fig139/fig139.component';
   ],
   imports: [
     CommonModule,
-    Module4RoutingModule
+    Module4RoutingModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    BusPubLibModule,
+    PlayerModule
+
   ]
 })
 export class Module4Module { }
