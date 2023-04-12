c6acc183f6082ccfa3a37e4f2871076a8e846f90
Noel Lotz <noel.lotz@pearson.com>
2023-04-12 15:14:19 -0500

    abel: updated titles in module 4 routing module.

--- busPub/projects/abel11-app/src/app/module4/module4-routing.module.ts
+++ busPub/projects/abel11-app/src/app/module4/module4-routing.module.ts
@@ -5,9 +5,9 @@ import { Fig135Component } from './fig135/fig135.component';
 import { Fig139Component } from './fig139/fig139.component';
 
 const routes: Routes = [
-  { path: 'figure13.4', component: Fig134Component, title: 'supply/demand for currency' },
-  { path: 'figure13.5', component: Fig135Component, title: 'goods market (open economy)' },
-  { path: 'figure13.9', component: Fig139Component, title: 'open economy ISLM' }
+  { path: 'figure13.4', component: Fig134Component, title: 'Supply/demand for currency' },
+  { path: 'figure13.5', component: Fig135Component, title: 'Goods market (open economy)' },
+  { path: 'figure13.9', component: Fig139Component, title: 'Open economy ISLM' }
 ];
 
 @NgModule({
