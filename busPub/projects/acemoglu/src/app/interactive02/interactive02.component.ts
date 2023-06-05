21ab521314333319dee917c040ea889e529b9962
Noel Lotz <noel.lotz@pearson.com>
2023-06-05 16:26:24 -0500

    acemoglu interactive 2 ready to review

--- busPub/projects/acemoglu/src/app/interactive02/interactive02.component.ts
+++ busPub/projects/acemoglu/src/app/interactive02/interactive02.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import * as Highcharts from 'highcharts';
@@ -8,12 +8,11 @@ import HC_sonify from 'highcharts/modules/sonification';
 import HC_annotate from 'highcharts/modules/annotations';
 import HC_labels from 'highcharts/modules/series-label';
 import HC_accessibility from 'highcharts/modules/accessibility';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRadioModule } from '@angular/material/radio';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 HC_export(Highcharts);
 HC_data(Highcharts);
@@ -25,7 +24,7 @@ HC_accessibility(Highcharts);
 @Component({
   selector: 'app-interactive02',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatInputModule],
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './interactive02.component.html',
   styleUrls: ['./interactive02.component.scss']
 })
@@ -35,22 +34,43 @@ export class Interactive02Component implements OnInit, AfterViewInit {
 
   xGood: string = 'Jeans';
   yGood: string = 'Sweaters';
+  budgetTitle: string = 'Clothing Budget'
   priceX: number = 50;
   priceY: number = 25;
-  income: number = 300;
+  income = signal(300);
+  minX = computed(() => {
+    let value = this.income() / 14;
+    return +value.toPrecision(4);
+  });
+  minY = computed(() => {
+    let value = this.income() / 20;
+    return +value.toPrecision(4);
 
+  });
+
+  budgetProps = new FormGroup(
+    {
+      priceX: new FormControl(50),
+      priceY: new FormControl(25),
+      income: new FormControl(300)
+    }
+  )
 
   constructor(announcer: LiveAnnouncer) {}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    this.budgetProps.valueChanges.subscribe((value) => {
+      this.priceX = value.priceX!;
+      this.priceY = value.priceY!;
+      if(value.income !== null) this.income.set(value.income!);
+      if(this.priceX !== null && this.priceY !== null && this.income !== null ) this._updateGraph();
+
+    });
+
 
   }
 
   ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
     let series = this._createSeries();
     this.chart = new Highcharts.Chart('chart1', {
       chart: {
@@ -61,14 +81,14 @@ export class Interactive02Component implements OnInit, AfterViewInit {
         animation: false,
       },
       caption: {
-        text: `The budget line shows all possible combinations of ${this.xGood} and ${this.yGood} that can be purchased with a given income. ${this.xGood} are shown on the x-axis and ${this.yGood} are shown on the y-axis.`
+        text: `The budget constraint shows all possible bundle of ${this.xGood} and ${this.yGood} that could be purchased with a given income. ${this.xGood} are shown on the x-axis and ${this.yGood} are shown on the y-axis.`
       },
       credits: {
         text: `Pearson Education`,
         href: 'javascript:window.open("https://www.pearson.com/", "_blank")',
       },
       title: {
-        text: 'Budget Line',
+        text: 'Budget Constraint',
         style: {
           fontFamily: 'sans-serif',
           fontWeight: '300',
@@ -76,7 +96,7 @@ export class Interactive02Component implements OnInit, AfterViewInit {
 
         }
       },
-      legend: { enabled: false },
+      legend: { enabled: true },
       tooltip: { useHTML: true },
       sonification: {
         duration: 6000,
@@ -103,10 +123,19 @@ export class Interactive02Component implements OnInit, AfterViewInit {
       series: [
         {
           type: 'line',
-          name: 'Budget line',
+          name: 'Budget constraint',
           lineWidth: 2,
           color: '#0771BD',
-          data: series.budget
+          data: series.budget,
+          label: { enabled: false}
+        },
+        {
+          type: 'area',
+          name: 'Budget set',
+          zIndex: -1,
+          color: '#FFC9B2',
+          data: series.budget,
+          visible: true
         }
       ],
       xAxis: {
@@ -131,7 +160,7 @@ export class Interactive02Component implements OnInit, AfterViewInit {
         series: {
           marker: { enabled: false, symbol: 'circle', radius: 2 },
           tooltip: {
-            headerFormat: '',
+            headerFormat: '<b>{series.name}</b><br/>',
             pointFormat: `{point.x:.1f} ${this.xGood}<br/>{point.y:.1f} ${this.yGood}`
           },
         }
@@ -143,8 +172,8 @@ export class Interactive02Component implements OnInit, AfterViewInit {
   }
 
   private _createSeries() {
-    let px = this.priceX, py = this.priceY, Y = this.income, x = 0;
-    let series = [];
+    let px = this.priceX, py = this.priceY, Y = this.income(), x = 0;
+    let series = [], setSeries = [], maxX = Y/px, step = maxX/4;
 
    let budgetLine = (x: number): number => { return Y / py - px / py * x; }
 
@@ -153,15 +182,28 @@ export class Interactive02Component implements OnInit, AfterViewInit {
         x: x,
         y: budgetLine(x)
       }
-      series.push(point);
-      x++;
 
-    } while (x <= Y / px);
+      series.push(point);
+      x = x + step;
+
+    } while (x <= maxX );
 
     return {
       budget: series
     }
 
+  }
+
+  private _updateGraph() {
+    let series = this._createSeries();
+    this.chart.series[0].update({
+      type: 'line',
+      data: series.budget
+    }, false);
+    this.chart.series[1].update({
+      type: 'area',
+      data: series.budget
+    }, true);
   }
 
 }
