1239c1dd22adf9fbd93ee85cd1acf00b89ba46fa
Noel Lotz <noel.lotz@pearson.com>
2023-07-02 16:25:51 -0500

    Acemoglu interactive 6: setup the basic structure

--- busPub/projects/acemoglu/src/app/interactive6/interactive6.component.ts
+++ busPub/projects/acemoglu/src/app/interactive6/interactive6.component.ts
@@ -1,13 +1,153 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { transition, trigger, style, animate } from '@angular/animations';
+import * as Highcharts from 'highcharts';
+import HC_more from 'highcharts/highcharts-more';
+import HC_export from 'highcharts/modules/exporting';
+import HC_data from 'highcharts/modules/data';
+import HC_sonify from 'highcharts/modules/sonification';
+import HC_annotate from 'highcharts/modules/annotations';
+import HC_labels from 'highcharts/modules/series-label';
+import HC_accessibility from 'highcharts/modules/accessibility';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+
+HC_more(Highcharts);
+HC_export(Highcharts);
+HC_data(Highcharts);
+HC_sonify(Highcharts);
+HC_annotate(Highcharts);
+HC_labels(Highcharts);
+HC_accessibility(Highcharts);
 
 @Component({
-  selector: 'app-interactive6',
+  selector: 'app-interactive5',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './interactive6.component.html',
-  styleUrls: ['./interactive6.component.scss']
+  styleUrls: ['./interactive6.component.scss'],
+  animations: [
+    trigger('myAnimationTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms 30ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0s', style({ opacity: 0 }))
+      ])
+    ])
+  ],
+
 })
-export class Interactive6Component {
+export class Interactive6Component implements OnInit, AfterViewInit {
+
+  mode = 0;
+  graphTitle: string = 'Market for Cheese';
+  xGood: string = 'cheese'
+  chart1!: Highcharts.Chart;
+
+
+  constructor(private el: ElementRef, private announcer: LiveAnnouncer) { }
+  
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this._setupStep();
+  }
+
+
+  // Private methods
+  private _setupStep() {
+    this._createSeries();
+    const container = this.el.nativeElement.querySelector('#chart1');
+    this.chart1 = new Highcharts.Chart(container, {
+      chart: {
+        height: 550,
+        shadow: { color: 'grey', offsetX: 1, offsetY: 1 },
+        borderRadius: 5,
+        animation: false,
+      },
+      caption: {
+        text: `The Wisconsin Cheeseman is a producer of cheese boxes. The graph shows the firm's cost curves and its demand curve. Per unit costs and market price are shown on the y-axis and the quantity produced is shown on the x-axis.`
+      },
+      credits: {
+        text: `Pearson Education`,
+        href: 'javascript:window.open("https://www.pearson.com/", "_blank")',
+      },
+      title: {
+        text: `${this.graphTitle}`,
+        style: {
+          fontFamily: 'sans-serif',
+          fontWeight: '300',
+          fontSize: '1.2em'
+
+        }
+      },
+      legend: { enabled: false },
+      tooltip: { useHTML: true, enabled: true },
+      sonification: {
+        duration: 6000,
+        afterSeriesWait: 1000,
+        defaultInstrumentOptions: {
+          instrument: 'piano',
+          mapping: {
+            pitch: {
+              min: 'c2',
+              max: 'c6',
+              scale: Highcharts.sonification.Scales?.majorPentatonic
+            }
+          }
+        },
+      },
+      accessibility: {
+        point: {
+          valueDescriptionFormat: `quantity: {point.x:.0f}, {point.name}: {point.y:.2f} dollars.`
+        },
+        keyboardNavigation: {
+          order: ['container', 'series', 'chartMenu']
+        }
+      },
+      series: [
+
+      ],
+      xAxis: {
+        lineColor: '#757575',
+        lineWidth: 1.,
+        tickColor: '#757575',
+        title: { useHTML: true, text: `Quantity of ${this.xGood} boxes ` },
+        min: 0,
+        max: 2000,
+      },
+      yAxis: {
+        gridLineWidth: 0,
+        lineColor: '#757575',
+        lineWidth: 1.,
+        tickColor: '#757575',
+        tickWidth: 1,
+        title: { useHTML: true, text: `Cost (price) per cheese box` },
+        min: .5,
+        max: 1.5,
+      },
+      plotOptions: {
+        series: {
+          marker: { enabled: false, symbol: 'circle', radius: 2 },
+          tooltip: {
+            headerFormat: '<b>{series.name}: </b> ',
+            pointFormat: `\${point.y:,.2f}<br/><b>Quantity:</b> {point.x:.0f}`
+          },
+          label: { enabled: true }
+        }
+      },
+
+    });
+
+  
+  }
+  
+  private _createSeries() {
+
+  }
 
 }
+
