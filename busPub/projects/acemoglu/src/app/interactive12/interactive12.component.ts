4fbc5e177fefc8adf6336a68c4b4cf0ae392191e
Noel Lotz <noel.lotz@pearson.com>
2023-08-14 18:32:43 -0500

    acemoglu interactive 12:  setting up graph and model.

--- busPub/projects/acemoglu/src/app/interactive12/interactive12.component.ts
+++ busPub/projects/acemoglu/src/app/interactive12/interactive12.component.ts
@@ -1,13 +1,190 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, signal, computed, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { transition, trigger, style, animate } from '@angular/animations';
+import * as Highcharts from 'highcharts';
+import HC_more from 'highcharts/highcharts-more';
+import HC_export from 'highcharts/modules/exporting';
+import HC_data from 'highcharts/modules/data';
+import HC_sonify from 'highcharts/modules/sonification';
+import HC_annotate from 'highcharts/modules/annotations';
+import HC_labels from 'highcharts/modules/series-label';
+import HC_accessibility from 'highcharts/modules/accessibility';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatRadioModule } from '@angular/material/radio';
+import { BusPubLibModule } from 'bus-pub-lib';
+import { ModelService, EconModel } from 'projects/poe-app/src/app/model.service';
+
+HC_more(Highcharts);
+HC_export(Highcharts);
+HC_data(Highcharts);
+HC_sonify(Highcharts);
+HC_annotate(Highcharts);
+HC_labels(Highcharts);
+HC_accessibility(Highcharts);
+
 
 @Component({
   selector: 'app-interactive12',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './interactive12.component.html',
-  styleUrls: ['./interactive12.component.scss']
+  styleUrls: ['./interactive12.component.scss'],
+  animations: [
+    trigger('myAnimationTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms 30ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0s', style({ opacity: 0 }))
+      ])
+    ])
+  ],
 })
-export class Interactive12Component {
+export class Interactive12Component implements OnInit, AfterViewInit {
+
+  chart1!: Highcharts.Chart;
+  mode = signal(0);
+
+  graph = signal(
+    {
+      xGood: 'Quantity (plates per day)',
+      yGood: 'Price per plate',
+      title: 'Market for Jambalaya',
+      caption: 'The market for jambalaya in equilbrium.',
+      xMin: 0,
+      xMax: 125,
+      yMin: 0,
+      yMax: 110,
+      xscale: 10,
+      yscale: .1
+    }
+  );
+  keyValues: any = {
+    yscale: .1,
+    xscale: 10
+  }
+
+  modelParams: Signal<EconModel> = computed(() => {
+    return {
+      xMin: 0,
+      xMax: 80,
+      step: 10,
+      xscale: this.graph().xscale,
+      yscale: this.graph().yscale,
+      demandIntercept: 100,
+      demandSlope: 1,
+    }
+  });
+
+
+
+  constructor(private el: ElementRef, private announcer: LiveAnnouncer, private modelService: ModelService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this._setupGraph();
+    
+  }
+
+
+
+
+  private _setupGraph() {
+    const container = this.el.nativeElement.querySelector('#chart1');
+    let series = this.modelService.monopolyModel({});
+    this.chart1 = new Highcharts.Chart(container, {
+      chart: {
+        height: 550,
+        shadow: { color: 'grey', offsetX: 1, offsetY: 1 },
+        borderRadius: 5,
+        animation: false,
+      },
+      caption: {
+        text: this.graph().caption
+      },
+      credits: {
+        text: `Pearson Education`,
+        href: 'javascript:window.open("https://www.pearson.com/", "_blank")',
+      },
+      title: {
+        text: `${this.graph().title}`,
+        style: {
+          fontFamily: 'sans-serif',
+          fontWeight: '300',
+          fontSize: '1.2em'
+
+        }
+      },
+      legend: { enabled: false },
+      tooltip: { useHTML: true, enabled: true },
+      sonification: {
+        duration: 6000,
+        afterSeriesWait: 1000,
+        defaultInstrumentOptions: {
+          instrument: 'piano',
+          mapping: {
+            pitch: {
+              min: 'c2',
+              max: 'c6',
+              scale: Highcharts.sonification.Scales?.majorPentatonic
+            }
+          }
+        },
+      },
+      accessibility: {
+        point: {
+          valueDescriptionFormat: `quantity: {point.x:.0f}, {point.name}: {point.y:.2f} dollars.`
+        },
+        keyboardNavigation: {
+          order: ['container', 'series', 'chartMenu']
+        }
+      },
+      series: [
+        {
+          type: 'line',
+          name: 'Demand',
+          data: series.demand
+        }
+
+      ],
+      xAxis: {
+        lineColor: '#757575',
+        lineWidth: 1.,
+        tickColor: '#757575',
+        title: { useHTML: true, text: `${this.graph().xGood}` },
+        min: this.graph().xMin,
+        max: this.graph().xMax
+      },
+      yAxis: {
+        gridLineWidth: 0,
+        lineColor: '#757575',
+        lineWidth: 1.,
+        tickColor: '#757575',
+        tickWidth: 1,
+        title: { useHTML: true, text: `${this.graph().yGood}` },
+        min: this.graph().yMin,
+        max: this.graph().yMax
+      },
+      plotOptions: {
+        series: {
+          marker: { enabled: false, symbol: 'circle', radius: 2 },
+          tooltip: {
+            headerFormat: '<b>{series.name}: </b> ',
+            pointFormat: `\${point.y:,.2f}<br/><b>Quantity:</b> {point.x:.0f}`
+          },
+          label: { useHTML: true, enabled: true, style: { fontSize: '.75em' } }
+        }
+      },
+
+
+    });
+
+
+  }
+
 
 }
