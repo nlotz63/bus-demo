d1b78a6575d76744944640d1e83cf845736892c8
Noel Lotz <noel.lotz@pearson.com>
2023-10-20 18:21:02 -0500

    hubbard9 interactive 4: integration of poe inter- actives 4 and 9 is nearly complete.

--- busPub/projects/hubbard9-app/src/app/interactiveho04/interactiveho04.component.ts
+++ busPub/projects/hubbard9-app/src/app/interactiveho04/interactiveho04.component.ts
@@ -10,8 +10,8 @@ import HC_annotate from 'highcharts/modules/annotations';
 import HC_labels from 'highcharts/modules/series-label';
 import HC_accessibility from 'highcharts/modules/accessibility';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { BusPubLibModule } from 'bus-pub-lib';
+import { MatRadioModule } from '@angular/material/radio';
 
 HC_more(Highcharts);
 HC_export(Highcharts);
@@ -24,54 +24,180 @@ HC_accessibility(Highcharts);
 @Component({
   selector: 'app-interactiveho04',
   standalone: true,
-  imports: [CommonModule, BusPubLibModule, ReactiveFormsModule, CurrencyPipe ],
+  imports: [CommonModule, BusPubLibModule, CurrencyPipe, MatRadioModule],
   templateUrl: './interactiveho04.component.html',
   styleUrls: ['./interactiveho04.component.scss'],
   animations: [
     trigger('myAnimationTrigger', [
-        transition(':enter', [
-            style({ opacity: 0 }),
-            animate('400ms 30ms ease-in', style({ opacity: 1 }))
-        ]),
-        transition(':leave', [
-            animate('0s', style({ opacity: 0 }))
-        ])
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('400ms 30ms ease-in', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0s', style({ opacity: 0 }))
+      ])
     ])
-],
+  ],
 
 })
 export class Interactiveho04Component implements OnInit, AfterViewInit {
 
   chart!: Highcharts.Chart;
-  xGood: string = 'Jeans';
-  equation1 = ``;
-  equation2 = ``;
-  equation3 = ``;
-  equationS1 = ``;
-  equationS2 = ``;
-  equationS3 = ``;
-  currencyFormat = new CurrencyPipe('en-US');
+  currencyFormat = new CurrencyPipe('en-US', 'USD');
   cs0Area = 'A';
   cs1Area = 'A';
 
-  sliderGroup = new FormGroup({
-    initialPrice: new FormControl(2),
-    newPrice: new FormControl(2)
+  // signals
+  mode = signal(0);
+  initialPrice = signal(2);
+  newPrice = signal(2);
+
+  controls = computed(() => {
+    return {
+      initPmin: this.mode() === 0 ? 1 : .5,
+      initPmax: this.mode() === 0 ? 5 : 3,
+      newPmin: this.mode() === 0 ? .5 : .25,
+      newPmax: this.mode() === 0 ? 6 : 3.5,
+    }
   });
 
+  graph = computed(() => {
+
+    if (this.mode() === 0) {
+
+      return {
+        title: `Demand for Chai Tea`,
+        caption: `The market demand curve for chai tea with a shaded triangle showing the consumer surplus at the initial price of $2.`,
+        xMax: 24,
+        yMax: 12
+      }
+      
+    } else {
+      return {
+        title: `Supply of Chai Tea`,
+        caption: `The market supply curve for chai tea with a shaded triangle showing the producer surplus at the initial price of $2.`,
+        xMax: 42,
+        yMax: 6
+
+      }
+    }
+  });
+
+  equations = signal({
+    one: ``,
+    two: ``,
+    three: ``
+})
 
 
   constructor(private announcer: LiveAnnouncer, private el: ElementRef) { }
 
   ngOnInit(): void {
-    this._createSeries();
-
+    this._setupGraph();
   }
 
   ngAfterViewInit(): void {
+  }
+
+  public updateGraph() {
+    let series = this._createSeries();
+    let cs1Description = this.initialPrice() >= this.newPrice() ? 'initial price' : 'new price';
+
+    if (this.initialPrice() > this.newPrice()) {
+      this.cs1Area = 'A + B + C';
+      this.cs0Area = 'A';
+    } else if (this.initialPrice() < this.newPrice()) {
+      this.cs1Area = 'A';
+      this.cs0Area = 'A + B + C';
+    } else {
+      this.cs0Area = 'A';
+      this.cs1Area = 'A';
+    }
+    let caption = `The market demand curve for jeans. The initial consumer surplus (CS<sub>0</sub>) is the area ${this.cs0Area}. The new consumer surplus (CS<sub>1</sub>) is the area ${this.cs1Area}.
+    `;
+
+    this.chart.update({
+      caption: {
+        text: caption,
+        useHTML: true
+      },
+      series: [
+        {
+          type: 'line',
+          data: this.mode() === 0 ? series.demand : series.supply
+        },
+        {
+          type: 'line',
+          data: series.initialPrice
+        },
+        {
+          type: 'line',
+          data: series.newPrice
+        },
+        {
+          type: 'arearange',
+          data: this.mode() === 0 ? series.CS1 : series.PS1,
+          accessibility: {
+            description: `The area under the demand curve above the ${cs1Description}.`
+          }
+        },
+        {
+          type: 'arearange',
+          data: this.mode() === 0 ? series.CS2 : series.PS2,
+        },
+        {
+          type: 'line',
+          data: this.mode() === 0 ? [] : series.connector,
+        },
+
+      ],
+      annotations: [
+        {
+          labels: [
+            {
+              point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: series.labelPositioner.xCSA,
+                y: series.labelPositioner.yCSA
+              },
+              text: 'A'
+            }
+          ]
+        },
+        {
+          visible: this.initialPrice() !== this.newPrice() ? true : false,
+          labels: [
+            {
+              point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: series.labelPositioner.xCSB,
+                y: series.labelPositioner.yCSB
+              },
+              text: 'B'
+            },
+            {
+              point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: series.labelPositioner.xCSC,
+                y: series.labelPositioner.yCSC
+              },
+              text: 'C'
+            }
+          ]
+        }
+      ]
+    }, true);
+    this.announcer.announce('The graph has been updated');
+  }
+
+  public _setupGraph() {
+    this.newPrice.set(2), this.initialPrice.set(2);
     let series = this._createSeries();
     const container = this.el.nativeElement.querySelector('#chart1');
-    this.chart = new Highcharts.Chart( container, {
+    this.chart = new Highcharts.Chart(container, {
       chart: {
         height: 550,
         shadow: { color: 'grey', offsetX: 1, offsetY: 1 },
@@ -80,14 +206,14 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
       },
       caption: {
         useHTML: true,
-        text: `The market demand curve for ${this.xGood} with a shaded triangle showing the consumer surplus at the initial price of $50.`
+        text: this.graph().caption
       },
       credits: {
         text: `Pearson Education`,
         href: 'javascript:window.open("https://www.pearson.com/", "_blank")',
       },
       title: {
-        text: `Demand for ${this.xGood}`,
+        text: this.graph().title,
         style: {
           fontFamily: 'sans-serif',
           fontWeight: '300',
@@ -112,7 +238,7 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           lineWidth: 2,
           zIndex: 1,
           color: '#0771BD',
-          data: series.demand,
+          data: this.mode() === 0 ? series.demand : series.supply,
           label: {
             useHTML: true,
             enabled: true
@@ -129,7 +255,7 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           zIndex: 2,
           color: 'black',
           data: series.initialPrice,
-          label: {enabled: false}
+          label: { enabled: false }
         },
         {
           type: 'line',
@@ -139,7 +265,7 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           zIndex: 2,
           color: 'black',
           data: series.newPrice,
-          label: {enabled: false}
+          label: { enabled: false }
         },
         {
           type: 'arearange',
@@ -147,7 +273,7 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           name: 'Area A',
           opacity: .5,
           zIndex: -1,
-          data: series.CS1,
+          data: this.mode() === 0 ? series.CS1 : series.PS1,
           accessibility: {
             description: `The area under the demand curve above the initial price.`
           }
@@ -159,27 +285,31 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           color: 'salmon',
           opacity: .4,
           zIndex: -1,
-          data: series.CS2,
+          data: this.mode() === 0 ? series.CS2 : series.PS2,
           accessibility: {
             description: `The area between the initial price, the new price and the demand curve.`
           }
         },
         {
           type: 'line',
-          name: 'S<sub>Market</sub>',
-          data: series.supply,
-          label: { enabled: true, useHTML: true}
-        }
+          dashStyle: 'ShortDash',
+          name: '',
+          lineWidth: 1.5,
+          zIndex: -1,
+          color: 'black',
+          data: series.connector,
+          label: {enabled: false}
+        },
 
       ],
       xAxis: {
         lineColor: '#757575',
         lineWidth: 1.,
         tickColor: '#757575',
-        title: { useHTML: true, text: `Quantity of ${this.xGood} ` },
+        title: { useHTML: true, text: `Quantity (thousands of cups  per day)` },
         min: 0,
-        max: 40,
-        tickInterval: 2
+        max: this.graph().xMax,
+        tickInterval: 5
       },
       yAxis: {
         gridLineWidth: 0,
@@ -187,10 +317,9 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
         lineWidth: 1.,
         tickColor: '#757575',
         tickWidth: 1,
-        title: { useHTML: true, text: `Price per pair` },
+        title: { useHTML: true, text: `Price (dollars per cup)` },
         min: 0,
-        max: 6,
-        tickInterval: 1,
+        max: this.graph().yMax,
       },
       plotOptions: {
         series: {
@@ -199,7 +328,7 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
             headerFormat: '<b>{series.name}</b><br/>',
             pointFormat: `\${point.y:.2f}`
           },
-          label: {enabled: false}
+          label: { enabled: false }
         }
       },
       annotations: [
@@ -255,109 +384,16 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
         }
       ]
     });
-
-  }
-
-  public updateGraph() {
-    let series = this._createSeries();
-    let slider = this.sliderGroup.value;
-    let cs1Description = slider.initialPrice! >= slider.newPrice! ? 'initial price' : 'new price';
-
-    if (slider.initialPrice! > slider.newPrice!) {
-      this.cs1Area = 'A + B + C';
-      this.cs0Area = 'A';
-    } else if (slider.initialPrice! < slider.newPrice!) {
-      this.cs1Area = 'A';
-      this.cs0Area = 'A + B + C';
-    } else {
-      this.cs0Area = 'A';
-      this.cs1Area = 'A';
-    }
-    let caption = `The market demand curve for jeans. The initial consumer surplus (CS<sub>0</sub>) is the area ${this.cs0Area}. The new consumer surplus (CS<sub>1</sub>) is the area ${this.cs1Area}.
-    `;
-
-    this.chart.update({
-      caption: {
-        text: caption,
-        useHTML: true
-      },
-      series: [
-        {
-          type: 'line',
-          data: series.demand
-        },
-        {
-          type: 'line',
-          data: series.initialPrice
-        },
-        {
-          type: 'line',
-          data: series.newPrice
-        },
-        {
-          type: 'arearange',
-          data: series.CS1,
-          accessibility: {
-            description: `The area under the demand curve above the ${cs1Description}.`
-          }
-        },
-        {
-          type: 'arearange',
-          data: series.CS2
-        },
-      ],
-      annotations: [
-        {
-          labels: [
-            {
-              point: {
-                xAxis: 0,
-                yAxis: 0,
-                x: series.labelPositioner.xCSA,
-                y: series.labelPositioner.yCSA
-              },
-              text: 'A'
-            }
-          ]
-        },
-        {
-          visible: slider.initialPrice! !== slider.newPrice! ? true : false,
-          labels: [
-            {
-              point: {
-                xAxis: 0,
-                yAxis: 0,
-                x: series.labelPositioner.xCSB,
-                y: series.labelPositioner.yCSB
-              },
-              text: 'B'
-            },
-            {
-              point: {
-                xAxis: 0,
-                yAxis: 0,
-                x: series.labelPositioner.xCSC,
-                y: series.labelPositioner.yCSC
-              },
-              text: 'C'
-            }
-          ]
-        }
-      ]
-    }, true);
-    this.announcer.announce('The graph has been updated');
-  }
+ }
 
   private _createSeries() {
 
     let a = 10, b = .5, c = 0, d = .125, x = 0;
-    let initP = this.sliderGroup.value.initialPrice!, newP = this.sliderGroup.value.newPrice!;
+    let initP = this.initialPrice(), newP = this.newPrice();
 
     let f = (x: number) => { return a - b * x; }, inverse = (x: number) => a / b - x / b;
     let s = (x: number) => { return c + d * x; }, inverseS = (x: number) => -c / d + x / d;
-
-
-    let demand: any[] = [],  supply: any[] = [ ], cs1: any[] = [], cs2: any[] = [];
+    let demand: any[] = [], supply: any[] = [], cs1: any[] = [], cs2: any[] = [];
 
     do {
       let point = {
@@ -411,10 +447,10 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
       {
         x: 0,
         high: newP > initP ? initP : newP,
-        low: f(0)
+        low: s(0)
       },
       {
-        x: newP > initP ? inverse(initP) : inverse(newP),
+        x: newP > initP ? inverseS(initP) : inverseS(newP),
         low: newP > initP ? initP : newP,
         high: newP > initP ? initP : newP
       }
@@ -427,70 +463,64 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
           high: newP > initP ? initP : newP
         },
         {
-          x: newP > initP ? inverse(initP) : inverse(newP),
+          x: newP > initP ? inverseS(initP) : inverseS(newP),
           low: newP > initP ? newP : initP,
           high: newP > initP ? initP : newP
         },
         {
-          x: newP > initP ? inverse(newP) : inverse(initP),
+          x: newP > initP ? inverseS(newP) : inverseS(initP),
           low: newP > initP ? newP : initP,
           high: newP > initP ? newP : initP
         }
       ];
-    
-      let connector = newP <= initP ? [{ x: inverse(newP), y: newP }, { x: inverse(newP), y: initP }] : [{ x: inverse(initP), y: initP }, { x: inverse(initP), y: newP}];
+
+    let connector = newP <= initP ? [{ x: inverseS(newP), y: newP }, { x: inverseS(newP), y: initP }] : [{ x: inverseS(initP), y: initP }, { x: inverseS(initP), y: newP }];
 
 
 
     let initPrice = [
       { x: 0, y: initP, accessibility: { enabled: false } },
       {
-        x: inverseS(initP), y: initP,
+        x: this.mode() === 0 ? inverse(initP) : inverseS(initP), y: initP,
         marker: {
           enabled: true,
           symbol: 'circle',
           radius: 4,
-          fillColor: 'rgb(235, 235, 235)',
+          fillColor: '#FCFBF2',
           lineWidth: 1,
           lineColor: 'black'
         }
       },
-      { x: inverseS(initP), y: 0, accessibility: { enabled: false } },
+      { x:this.mode() === 0 ? inverse(initP) : inverseS(initP), y: 0, accessibility: { enabled: false } },
     ];
     let newPrice = [
       { x: 0, y: newP, accessibility: { enabled: false } },
       {
-        x: inverseS(newP), y: newP,
+        x: this.mode() === 0 ? inverse(newP) : inverseS(newP), y: newP,
         marker: {
           enabled: true,
           symbol: 'circle',
           radius: 4,
-          fillColor: 'rgb(235, 235, 235)',
+          fillColor: '#FCFBF2',
           lineWidth: 1,
           lineColor: 'black'
         }
       },
-      { x: inverseS(newP), y: 0, accessibility: { enabled: false } },
+      { x: this.mode() === 0 ? inverse(newP) : inverseS(newP), y: 0, accessibility: { enabled: false } },
     ];
     // dynamic equations
     let cs0 = ((f(0) - initP) * inverse(initP)) / 2, csNew = ((f(0) - newP) * inverse(newP)) / 2, csDif = csNew - cs0;
     let ps0 = ((initP - s(0)) * inverse(initP)) / 2, psNew = ((newP - s(0)) * inverse(newP)) / 2, psDif = psNew - ps0;
 
+    this.equations().one = this.mode() === 0 ? `$$ CS_0 = \\frac{(\$${f(0)}- ${this.currencyFormat.transform(initP)}) \\times (${inverse(initP).toFixed(1)}-0)} {2} = ${this.currencyFormat.transform(cs0)} $$` : `$$ PS_0 = \\frac{(\$${initP.toFixed(2)} - \$${s(0)}) \\times (${inverseS(initP).toFixed(1)}-0)} {2} = ${this.currencyFormat.transform(ps0)} $$`;
 
-    this.equation1 = `$$ CS_0 = \\frac{(\$${f(0)}- \$${initP}) \\times (${inverse(initP)}-0)} {2} = ${this.currencyFormat.transform(cs0) } $$`;
-    this.equation2 = `$$ CS_1 = \\frac{(\$${f(0)}- \$${newP}) \\times (${inverse(newP)}-0)} {2} = ${ this.currencyFormat.transform(csNew)} $$`;
-    this.equation3 = `$$\\text{Change in consumer surplus} = CS_1 - CS_0 = ${this.currencyFormat.transform(csDif)} $$`;
+    this.equations().two = this.mode() === 0 ? `$$ CS_1 = \\frac{(\$${f(0)}- \$${newP.toFixed(2)}) \\times (${inverse(newP).toFixed(1)}-0)} {2} = ${this.currencyFormat.transform(csNew)} $$` : `$$ PS_1 = \\frac{(\$${newP.toFixed(2)} - \$${s(0)}) \\times (${inverseS(newP).toFixed(1)}-0)} {2} = ${this.currencyFormat.transform(psNew)} $$`;
+
+    this.equations().three = this.mode() === 0 ? `$$\\Delta CS = CS_1 - CS_0 = ${this.currencyFormat.transform(csDif)} \\text{ thousand} $$` : `$$\\Delta PS = PS_1 - PS_0 = ${this.currencyFormat.transform(psDif)} \\text{ thousand} $$`;
 
     let labelPosition = this._positioner(initP, newP, inverse(initP), inverse(newP));
 
-    this.equationS1 = `$$ PS_0 = \\frac{(\$${initP} - \$${f(0)}) \\times (${inverse(initP)}-0)} {2} = ${this.currencyFormat.transform(ps0) } $$`;
-    this.equationS2 = `$$ PS_1 = \\frac{(\$${newP} - \$${f(0)}) \\times (${inverse(newP)}-0)} {2} = ${ this.currencyFormat.transform(psNew)} $$`;
-    this.equationS3 = `$$\\text{Change in producer surplus} = PS_1 - PS_0 = ${this.currencyFormat.transform(psDif)} $$`;
-
     let labelPositionS = this._positioner(initP, newP, inverseS(initP), inverseS(newP));
-
-
-
 
     return {
       demand: demand,
@@ -515,25 +545,25 @@ export class Interactiveho04Component implements OnInit, AfterViewInit {
       xcsA = newQ / 2;
       xcsB = newQ / 2;
       xcsC = (initQ + newQ) / 2;
-      ycsA = newP + 5;
-      ycsB = initP+1;
-      ycsC = initP+1;
+      ycsA = newP + .5;
+      ycsB = initP + .1;
+      ycsC = initP + .1;
 
     } else if (initP > newP) {
       xcsA = initQ / 2;
       xcsB = initQ / 2;
       xcsC = (initQ + newQ) / 2;
-      ycsA = initP + 5;
-      ycsB = newP+1;
-      ycsC = newP+1;
+      ycsA = initP + .5;
+      ycsB = newP + .1;
+      ycsC = newP + .1;
 
     } else {
       xcsA = newQ / 2;
       xcsB = newQ / 2;
       xcsC = (initQ + newQ) / 2;
-      ycsA = newP + 5;
-      ycsB = initP+1;
-      ycsC = initP+1;
+      ycsA = newP + .5;
+      ycsB = initP + .1;
+      ycsC = initP + .1;
 
     }
 
